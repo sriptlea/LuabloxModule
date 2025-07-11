1ec25d1e5457d64222a47372ba440b19b61a06e0
Script_hacker <xxxxitscoolxxxx@gmail.com>
2025-07-11 14:47:34 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const noblox = require('noblox.js')
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/Shout', function(req, res) {
+    const groupid = req.query.groupid
+  const cookie = req.query.cookie
+  const shoutstring = req.query.shoutstring.replace("+", " ")
+  if(!groupid || !cookie || !target) return
+  noblox.setCookie(cookie).then(() => {
+    noblox.shout(groupid, shoutstring).catch(() => {"There was an error while shouting at that specific group."})
+  }).catch(() => {
+    console.warn("An invalid cookie was provided.")
+  })
+})
+
+app.get('/setRank', function(req, res) {
+  const groupid = req.query.groupid
+  const cookie = req.query.cookie
+  const idtoRank = req.query.rankid
+  const target = req.query.target
+  if(!groupid || !cookie || !target) return
+  noblox.setCookie(cookie).then(() => {
+    noblox.setRank(groupid, target, idtoRank).catch(() => {
+      console.warn("There was an error while ranking that user.")
+    })
+  }).catch(() => {
+    console.warn("An invalid cookie was provided.")
+  })
+})
+
+app.get('/Promote', function(req, res) {
+  const groupid = req.query.groupid
+  const cookie = req.query.cookie
+  const target = req.query.target
+  if(!groupid || !cookie || !target) return
+  noblox.setCookie(cookie).then(() => {
+    noblox.promote(groupid, target).catch(() => {
+      console.warn("There was an error while ranking that user.")
+    })
+  }).catch(() => {
+    console.warn("An invalid cookie was provided.")
+  })
+})
+
+app.get('/Demote', function(req, res) {
+  const groupid = req.query.groupid
+  const cookie = req.query.cookie
+  const target = req.query.target
+  noblox.setCookie(cookie).then(() => {
+    noblox.demote(groupid, target).catch(() => {
+      console.warn("There was an error while ranking that user.")
+    })
+  }).catch(() => {
+    console.warn("An invalid cookie was provided.")
+  })
+})
